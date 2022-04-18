8860b2a5f57324d19f01e2cac83ff629365e2188
mcleanka <mcleankasambala@gmail.com>
2022-04-18 16:39:05 +0200

    code refactoring

--- create.py
+++ create.py
@@ -1,27 +1,4 @@
-import sqlite3
-
-"""
-Defining variables
-"""
-# database name
-schema = "university"
-# database file name
-database_name = f"{schema}.db"
-
-# database table names
-departments = "department"  # stores db departments
-instructors = "instructors"  # stores db instructors
-
-# creating a connection to the database
-# sqlite3 object will make connection and
-# create database schema with the filename
-# {{ university.db }}
-connection = sqlite3.connect(database_name)
-
-# database exception handling
-with connection:
-    # create a cursor object
-    cursor = connection.cursor()
+from db import cursor, departments, instructors
 
 # sql statements to create tables
 sql = [
