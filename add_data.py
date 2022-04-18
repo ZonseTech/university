34760f2a183794ae86d2a0a384a2f41888d2a83d
mcleanka <mcleankasambala@gmail.com>
2022-04-18 17:16:58 +0200

    code refactoring

---
+++ add_data.py
@@ -0,0 +1,20 @@
+from db import cursor, departments, instructors
+
+# sql statements to create tables
+sql = [
+    f"CREATE TABLE IF NOT EXISTS {departments} (dept_name TEXT PRIMARY KEY, building TEXT, budget NUMERIC)",
+    f"CREATE TABLE IF NOT EXISTS {instructors} ("
+    f"ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+    f"name TEXT,"
+    f"dept_name TEXT,"
+    f"salary NUMERIC,"
+    f"FOREIGN KEY(dept_name) REFERENCES {departments}(dept_name))",
+]
+
+try:
+    # looping though sql statements
+    for stm in sql:
+        # execute SQL query
+        cursor.execute(stm)
+except Exception as error:
+    print(error)
