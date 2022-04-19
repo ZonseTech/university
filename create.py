aae43622c22c3b0c6080268e9f3399ad18a2ca8b
mcleanka <mcleankasambala@gmail.com>
2022-04-19 22:34:11 +0200

    fetched instructors

--- create.py
+++ create.py
@@ -1,20 +1,9 @@
-from db import cursor, departments, instructors
+from db import cursor, instructors, connection
 
-# sql statements to create tables
-sql = [
-    f"CREATE TABLE IF NOT EXISTS {departments} (dept_name TEXT PRIMARY KEY, building TEXT, budget NUMERIC)",
-    f"CREATE TABLE IF NOT EXISTS {instructors} ("
-    f"ID INTEGER PRIMARY KEY AUTOINCREMENT, "
-    f"name TEXT,"
-    f"dept_name TEXT,"
-    f"salary NUMERIC,"
-    f"FOREIGN KEY(dept_name) REFERENCES {departments}(dept_name))",
-]
 
-try:
-    # looping though sql statements
-    for stm in sql:
-        # execute SQL query
-        cursor.execute(stm)
-except Exception as error:
-    print(error)
+def save(params):
+    sql = f"INSERT INTO {instructors}(name, dept_name, salary) VALUES(?, ?, ?)"
+    cursor.execute(sql, params)
+    connection.commit()
+
+    return cursor
